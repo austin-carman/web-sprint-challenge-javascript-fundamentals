01e00f751b19bf42595c50aad0c2d583e57fdc24
Austin Carman <abcarman12@gmail.com>
2021-03-26 10:10:49 -0600

    finished topic 1, task 2

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ nestedFunction can access internal because nestedFunction is a closure and has a
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+  return num * (num + 1) / 2;
   }
+
+  // console.log('task 2:', summation(4));
  
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
